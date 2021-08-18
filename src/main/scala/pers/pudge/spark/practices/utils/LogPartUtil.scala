4ea4fad3fc232f031883e0206c59d33ade646590
pudgebd <pudgebd>
2021-08-18 11:24:45 +0800

    ++++

--- src/main/scala/pers/pudge/spark/practices/utils/LogPartUtil.scala
+++ src/main/scala/pers/pudge/spark/practices/utils/LogPartUtil.scala
@@ -1,11 +1,16 @@
 package pers.pudge.spark.practices.utils
 
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.{SaveMode, SparkSession}
 import org.apache.spark.sql.expressions.Window
 import org.apache.spark.sql.functions._
 
 object LogPartUtil {
 
+  val logPath = "/Users/chenqian/tmp/boot_2021-08-17.log"
+  val logBegin = "INFO 21/08/17 19"
+  val logEnd = "INFO 21/08/18 00"
+  val savePath = "/Users/chenqian/tmp/boot_2021-08-17_19dian_00dian.log"
+  
   def main(args: Array[String]): Unit = {
     val spark = SparkSession.builder.appName("LocalSpark")
       .master("local[*]")
@@ -14,7 +19,7 @@ object LogPartUtil {
     import spark.implicits._
 
     val randow = new util.Random()
-    val df = spark.read.textFile("/Users/chenqian/tmp/boot_2021-08-17.log")
+    val df = spark.read.textFile(logPath)
       .map(line => {
         (randow.nextInt(10), line)
       }).toDF("parti", "line")
@@ -23,14 +28,12 @@ object LogPartUtil {
     df.withColumn("row_number", row_number.over(windowSpec))
       .createOrReplaceTempView("tmp")
 
-    //94053 行开始
     val startRowNum = spark
-      .sql("select parti, row_number from tmp where substring(line, 0, 16) = 'INFO 21/08/17 19'")
+      .sql(s"select parti, row_number from tmp where substring(line, 0, 16) = '${logBegin}'")
       .collect()(0).getInt(1)
 
-    //181950 行结束
     val endRowNum = spark
-      .sql("select parti, row_number, line from tmp where substring(line, 0, 16) = 'INFO 21/08/18 00' limit 1")
+      .sql(s"select parti, row_number, line from tmp where substring(line, 0, 16) = '${logEnd}' limit 1")
       .collect()(0).getInt(1)
 
     //    val sql = "select substring(line, 0, 16) from tmp limit 10"
@@ -38,7 +41,8 @@ object LogPartUtil {
 
     val sql = s"select line from tmp where row_number >= $startRowNum and row_number <= $endRowNum"
     spark.sql(sql).coalesce(1)
-      .write.text("/Users/chenqian/tmp/boot_2021-08-17_19dian_00dian.log")
+      .write.mode(SaveMode.Overwrite)
+      .text(savePath)
   }
 
 }
